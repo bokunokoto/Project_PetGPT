2b1107f372453ffd123d550f631050ac93d72e9e
hyeonchang <hyeonchang88@gmail.com>
2026-05-27 07:36:04 +0000

    typo 수정

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="Pet-E2E: 반려동물 통합 케어", layout="wide")
+st.set_page_config(page_title="Pet-GPT: 반려동물 통합 케어", layout="wide")
 
 st.title("반려동물 생애주기 통합 관리 서비스")
-st.subheader("입양부터 마지막 순간까지, Pet-E2E가 함께합니다.")
+st.subheader("입양부터 마지막 순간까지, Pet-GPT가 함께합니다.")
 st.info("왼쪽 사이드바 메뉴를 선택하여 각 서비스를 이용해 보세요.")
